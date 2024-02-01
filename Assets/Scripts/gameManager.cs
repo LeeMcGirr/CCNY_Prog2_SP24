7565ae2ce2bf976022f78acb63b8eed2e3f13b33
MxGRRR <89796414+LeeMcGirr@users.noreply.github.com>
2024-02-01 16:48:19 -0500

    added collectible

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class gameManager : MonoBehaviour
 {
+    [Header("Global vars")]
+    public GameObject myPlayer;
+    public float timer;
+    public int score;
+
+    [Header("NPC vars")]
+    public GameObject collectible1;
+    public float spawnInterval;
+    public float spawnTimer;
+    public Vector2 spawnXBounds;
+    public Vector2 spawnYBounds;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,20 @@ public class gameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //timer is global, spawnTimer tracks collectibles
+        timer += Time.deltaTime;
+        spawnTimer += Time.deltaTime;
+
+        //this is the world position where our collectible spawns
+        float x = Random.Range(spawnXBounds.x,spawnXBounds.y);
+        float y = Random.Range(spawnYBounds.x,spawnYBounds.y);
+        Vector3 targetPos = new Vector3(x, y, 0);
+
+        //instantiate and reset timer when condition is met
+        if (spawnTimer > spawnInterval)
+        {
+            Instantiate(collectible1, targetPos, Quaternion.identity);
+            spawnTimer = 0;
+        }
     }
 }
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class playerController : MonoBehaviour
 {
     //speed is a multiplier used to control actual speed of player
     public float speed;
-    Vector3 myDir;
+    private Vector3 myDir;
+    int myScore;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,39 +18,33 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
-    }
-
-    void OnCollisionStay(Collision collision)
-    {
 
     }
 
-    public Vector3 GetMyDir()
+    void FixedUpdate()
     {
-        return myDir;
+        transform.Translate(Dir() * speed);
     }
 
-    public void Move()
+    public Vector3 Dir()
     {
-        //simple keycode checks - start with W for UP
-        if (Input.GetKey(KeyCode.W))
-        {
-            //for now we'll just directly call the transform and Translate it
-            transform.Translate(Vector3.up * speed);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * speed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(Vector3.down * speed);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * speed);
-        }
+        //referencing Unity's virtual axis - these pick up KBM OR controller inputs
+        float y = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        myDir = new Vector3(x, y, 0); //combining them into one vector
+        //Debug.Log(myDir);
+        return myDir; //return the value
+    }
 
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("other: " + collision.gameObject.name);
+        Debug.Log("other tag: " + collision.gameObject.tag);
+
+        if(collision.gameObject.tag == "Collectible")
+        {
+            Destroy(collision.gameObject);
+            myScore++;
+        }
     }
 }
